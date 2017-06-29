766adc7954912b919be2f6aee4732259075311a2
Prithvi Velpuri <pvelpur@purdue.edu>
2017-06-29 12:13:36 -0400

    Add a button (no functionality)

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
 const app = {
-    init: function(){
+    init: function(selectors){
+        this.flicks = []
         this.max = 0;
+        this.list = document.querySelector(selectors.listSelector)
         document
-            .querySelector('form#flick-form')
+            .querySelector(selectors.formSelector)
             .addEventListener('submit', this.handleSubmit.bind(this))
+    },
+
+    renderListItem: function(flick){
+        const item = document.createElement('li')
+        item.textContent = flick.name
+
+        const favoriteButton = document.createElement('button')
+        favoriteButton.textContent = 'Favorite!'
+        favoriteButton.addEventListener('click', this.handleFavoriteButton.bind(this))
+        item.appendChild(favoriteButton)
+
+        return item
     },
 
     handleSubmit: function(ev) {
         ev.preventDefault()
         const f = ev.target
-        const flick ={
+        const flick = {
             id: this.max + 1,
             name: f.flickName.value,
         }
-        console.log(flick)
+        //console.log(flick)
+
+        this.flicks.push(flick)
+
+        const listItem = this.renderListItem(flick)
+        this.list.appendChild(listItem)
+
         this.max++
+    },
+
+    handleFavoriteButton: function(ev){
+        ev.preventDefault()
+        const f = ev.target
     },
 }
 
-app.init()
+
+app.init({
+    formSelector: 'form#flick-form',
+    listSelector: '#flick-list'
+})
